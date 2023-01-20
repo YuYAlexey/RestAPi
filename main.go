635dde0867e4adf765426917fcf971318b1e2347
AlexeyYU <ad.yushinwork@gmail.com>
2023-01-20 16:19:35 +0300

    Create DB connect

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/adYushinW/RestAPi/connect"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	route := gin.Default()
+	connect.ConnectDB()
 
 	route.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
+		context.JSON(http.StatusOK, gin.H{"message": "Hello"})
 	})
 
 	route.Run()
