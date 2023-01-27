ef2011179997976caa60062c22717f3fa539a824
AlexeyYU <ad.yushinwork@gmail.com>
2023-01-27 16:46:31 +0300

    some fixes

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"net/http"
+	"test/api"
+	"test/connect/db"
 
-	"github.com/adYushinW/RestAPi/connect"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	route := gin.Default()
-	connect.ConnectDB()
+	d := &db.Database{}
+	d.ConnectDB()
 
 	route.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Hello"})
 	})
+
+	api := &api.Todoinfo{}
+	route.GET("/all", api.GetAll)
 
 	route.Run()
 }
